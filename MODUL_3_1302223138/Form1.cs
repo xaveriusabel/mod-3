f1f0273a5d82d7bd1d384be66a3a4b25b039d330
xaveriusabel <you@example.com>
2024-03-06 06:59:07 +0700

    first commit

---
+++ MODUL_3_1302223138/Form1.cs
@@ -0,0 +1,10 @@
+namespace MODUL_3_1302223138
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MODUL_3_1302223138/Program.cs
@@ -0,0 +1,17 @@
+namespace MODUL_3_1302223138
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
